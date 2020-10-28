047c53980812b81a9dd76913fb4dd5a3e5491ca6
denis matijevic <denisone90@gmail.com>
2020-10-28 02:31:58 -0600

    updated .env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,7 @@
 import firebase from "firebase/app"
 import "firebase/auth"
 
-const app = firebase.initializeApp({
-  apiKey: "AIzaSyDUEvDRGRm63LovqItENHwRnOSv0qGGNCU",
-  authDomain: "summerlin-labs-production.firebaseapp.com",
-  databaseURL: "https://summerlin-labs-production.firebaseio.com",
-  projectId: "summerlin-labs-production",
-  storageBucket: "summerlin-labs-production.appspot.com",
-  messagingSenderId: "1044214191843",
-  appId: "1:1044214191843:web:d8d43080fc77631493d7d6"
-})
+
 
 export const auth = app.auth()
 export default app
