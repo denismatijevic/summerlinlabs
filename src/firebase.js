f5be3e1e03eadb1fac9b870d1eea905fbcaaae39
Collin <collinhodgs@gmail.com>
2020-10-31 11:11:56 -0600

    Created calendar component, need to add google calendar api for it to work properly

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
-import firebase from "firebase/app"
-import "firebase/auth"
+import firebase from "firebase/app";
+import "firebase/auth";
 
 // ADD API KEY HERE
 
-export const auth = app.auth()
-export default app
+export const auth = app.auth();
+export default app;
