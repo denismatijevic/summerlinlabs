8ad3440898f6710018d869c8c3734b97f3e7b761
Collin <collinhodgs@gmail.com>
2020-11-05 14:00:19 -0700

    Fixing routing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import HomePage from "./HomePage";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={App} exact />
-      <Route path="/homepage" component={HomePage} exact />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
