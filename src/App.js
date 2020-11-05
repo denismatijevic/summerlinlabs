2c0695d0f9f2b059045449836081cef82f82c321
Jameson Midgley <jameson.midgley@gmail.com>
2020-11-05 16:59:18 -0700

    mission statement

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper/index";
 import Homepage from "./pages/Homepage";
 import Contact from "./pages/Contact";
+import MissionState from "./pages/Mission";
 import "./App.css";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/" component={Homepage} />
           <Route exact path="/catalog" component={Homepage} />
           <Route exact path="/contact" component={Contact} />
+          <Route exact path="/mission" component={MissionState} />
         </Wrapper>
         <Footer />
       </div>
