b18be0637bf36c37bac72a3e85246c77e84b3367
Collin <collinhodgs@gmail.com>
2020-11-05 03:14:11 -0700

    Added in searchable table to add all the single testing with their prices

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import LandingPage from "./components/LandingPage"
-import TestCatalog from "./components/TestCatalog"
-import Footer from './components/Footer'
-import Main from './Main'
+import React from "react";
+import LandingPage from "./components/LandingPage";
+import TestCatalog from "./components/TestCatalog";
+import Footer from "./components/Footer";
+import Main from "./Main";
 
 function App() {
   return (
@@ -12,8 +12,7 @@ function App() {
       <Main />
       <Footer />
     </>
-  )
+  );
 }
-
 
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import Signup from './components/Signup';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import Signup from "./components/Signup";
+import { Container } from "react-bootstrap";
 import { AuthProvider } from "./contexts/AuthContext";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Dashboard from "./components/Dashboard"
-import Login from "./components/Login"
-import PrivateRoute from './components/PrivateRoute'
-import ForgotPassword from "./components/ForgotPassword"
-import UpdateProfile from "./components/UpdateProfile"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import ForgotPassword from "./components/ForgotPassword";
+import UpdateProfile from "./components/UpdateProfile";
 
 export default function Main() {
   return (
-    <Container className="d-felx align-items-center justify-content-center"
+    <Container
+      className="d-felx align-items-center justify-content-center"
       style={{ minHeight: "100vh" }}
     >
       <div className="w-100" style={{ maxWidth: "400px" }}>
