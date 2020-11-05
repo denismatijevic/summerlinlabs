17d940b5361954308c445a48ec960fce600749dc
denis matijevic <denisone90@gmail.com>
2020-11-04 20:07:01 -0700

    updated auth

---
+++ src/components/LandingPage.js
@@ -0,0 +1,10 @@
+// import React, { useRef, useState } from 'react';
+// import { Form, Button, Card, Alert } from 'react-bootstrap';
+// import { useAuth } from '../contexts/AuthContext'
+// import { Link, useHistory } from "react-router-dom"
+
+// const LandingPage = () => {
+
+// }
+
+// export default LandingPage;
