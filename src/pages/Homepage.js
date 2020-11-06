d3d59dc7b20501fc59a6c47c291ab77f06a22acc
Kyle Pace <kylelpace@gmail.com>
2020-11-05 19:29:03 -0700

    adding professional treatments

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Wrapper from "../components/Wrapper/index";
 import Jumbotron from "../components/Jumbotron/Jumbotron";
-
+import InfoCards from "../components/InfoCards";
 function HomePage() {
   return (
     <Wrapper>
       <Jumbotron />
+      <InfoCards />
     </Wrapper>
   );
 }
