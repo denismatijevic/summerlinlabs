6ed5bcab4fdfb48bd843d7ea2ca72b07bc4df59d
denis matijevic <denisone90@gmail.com>
2020-11-11 18:09:59 -0700

    updated contact form with automated email message

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,18 +1,31 @@
 import React from "react";
+import emailjs from 'emailjs-com';
 import { MDBBtn, MDBIcon, MDBInput, MDBCard, MDBCardBody } from "mdbreact";
 
 const ContactForm = () => {
-  return (
-    <MDBCard
-      className="my-5 px-5 mx-auto bg"
-      style={{ fontWeight: 300, maxWidth: "80%" }}
-    >
-      <MDBCardBody>
-        <form>
+
+function sendEmail(e) {
+  e.preventDefault();
+
+  emailjs.sendForm('service_sa9by6b', 'template_1aql2q7', e.target, 'user_kg7FnuA649SwSPhBnhFdR')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+    e.target.reset()
+}
+
+return (
+  <MDBCard className="my-5 px-5 mx-auto bg"
+  style={{ fontWeight: 300, maxWidth: "80%" }}>
+        <MDBCardBody>
+         <form className="contact-form" onSubmit={sendEmail}>
           <p className="h5 text-center mb-4">Contact us!</p>
           <div className="grey-text">
             <MDBInput
               label="Your name"
+              name="name"
               icon="user"
               group
               type="text"
@@ -22,6 +35,7 @@ const ContactForm = () => {
             />
             <MDBInput
               label="Your email"
+              name="email"
               icon="envelope"
               group
               type="email"
@@ -31,6 +45,7 @@ const ContactForm = () => {
             />
             <MDBInput
               label="Subject"
+              name="subject"
               icon="tag"
               group
               type="text"
@@ -42,11 +57,12 @@ const ContactForm = () => {
               type="textarea"
               rows="2"
               label="Your message"
+              name="message"
               icon="pencil-alt"
             />
           </div>
           <div className="text-center">
-            <MDBBtn outline color="info">
+            <MDBBtn outline color="info" type="submit">
               Send
               <MDBIcon far icon="paper-plane" className="ml-1" />
             </MDBBtn>
@@ -54,7 +70,7 @@ const ContactForm = () => {
         </form>
       </MDBCardBody>
     </MDBCard>
-  );
-};
+);
+}
 
 export default ContactForm;
